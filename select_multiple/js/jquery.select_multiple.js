75dfb354c89f8d69f97b8460fe509f84e1890812
aladjev.andrew <aladjev.andrew@gmail.com>
2012-02-03 14:59:51 +0300

    select multiple init

---
+++ select_multiple/js/jquery.select_multiple.js
@@ -0,0 +1,314 @@
+/*
+jquery.select_multiple.js - custom multiselect
+Copyright (C) 2012 aladjev.andrew@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+(function($, Array) {
+	if (!('indexOf' in Array.prototype)) {
+		Array.prototype.indexOf= function(find, i /*opt*/) {
+			if (i===undefined) i= 0;
+			if (i<0) i+= this.length;
+			if (i<0) i= 0;
+			for (var n= this.length; i<n; i++)
+				if (i in this && this[i]===find)
+					return i;
+			return -1;
+		};
+	}
+	
+	$.widget("ui.SelectMultiple", {
+		options: {
+			sels: {
+				self:	".select_multiple",
+				list:	"ul",
+				item:	"li"
+			},
+			html: {
+				wrapper:	"<div class='select_multiple'></div>",
+				list:		"<ul></ul>",
+				item:		"<li></li>"
+			},
+			classes: {
+				active: "active"
+			}
+		},
+		
+		_create: function() {
+			var instance = this;
+			
+			this.select = this.element;
+			var width = this.element.outerWidth();
+			var height = this.element.outerHeight();
+			this.select.wrap(this.options.html.wrapper);
+			this.element = this.select.parent();
+			this.element.width(width);
+			this.select.hide();
+			this.all_options = this.select.find("option");
+			this.element.append(this.options.html.list);
+			
+			this.list = this.element.find(this.options.sels.list);
+			this.list.width(width).height(height);
+			this.all_options.each(function() {
+				instance.list.append(instance.options.html.item);
+				var item = instance.list.find(instance.options.sels.item).last();
+				item.text($(this).text());
+			});
+			this.all_items = this.list.find(this.options.sels.item);
+			
+			this.init();
+		},
+		
+		init: function() {
+			var instance = this;
+			this.current_index = this.select.find("option:selected").first().index();
+			this.select.find("option:selected").each(function() {
+				var index = $(this).index();
+				instance.all_items.eq(index).addClass(instance.options.classes.active);
+			});
+			this.store_active();
+			
+			var count = 0;
+			this.all_items.click(function(event) {
+				if(!instance.state_focus) {
+					return;
+				}
+				
+				count = 0;
+				var index = $(this).index();
+				if(event.shiftKey) {
+					count = index - instance.current_index;
+					instance.selection(instance.current_index, count);
+					instance.store_active();
+				} else if(event.ctrlKey) {
+					instance.toggle(index);
+					instance.current_index = instance.select.find("option:selected").first().index();
+					instance.store_active();
+				} else {
+					var change = false;
+					instance.select.find("option:selected").each(function() {
+						if(index != $(this).index()) {
+							instance.deactivate($(this).index());
+							change = true;
+						}
+					});
+					change = instance.activate(index) || change;
+					if(change) {
+						instance.select.trigger("change");
+					}
+					instance.current_index = index;
+					instance.store_active();
+				}
+			}).bind("selectstart dragstart", function(event) {
+				event.preventDefault();
+				return false;
+			});
+			
+			$(document).bind("keydown", "shift+up", function() {
+				if(!instance.state_focus) {
+					return;
+				}
+				
+				count--;
+				if(instance.current_index + count >= instance.all_items.size()) {
+					count = instance.all_items.size() - instance.current_index - 1;
+				} else if(instance.current_index + count < 0) {
+					count = - instance.current_index;
+				}
+				var change = instance.selection(instance.current_index, count, true);
+				change = instance.restore_active() || change;
+				instance.scroll_list_to(instance.current_index + count);
+				if(change) {
+					instance.select.trigger("change");
+				}
+			});
+			$(document).bind("keydown", "shift+down", function() {
+				if(!instance.state_focus) {
+					return;
+				}
+				
+				count++;
+				if(instance.current_index + count >= instance.all_items.size()) {
+					count = instance.all_items.size() - instance.current_index - 1;
+				} else if(instance.current_index + count < 0) {
+					count = - instance.current_index;
+				}
+				var change = instance.selection(instance.current_index, count, true);
+				change = instance.restore_active() || change;
+				instance.scroll_list_to(instance.current_index + count);
+				if(change) {
+					instance.select.trigger("change");
+				}
+			});
+			$(document).bind("keydown", "up", function() {
+				if(!instance.state_focus) {
+					return;
+				}
+				
+				count = 0;
+				instance.current_index--;
+				if(instance.current_index >= instance.all_items.size()) {
+					instance.current_index = instance.all_items.size() - 1;
+				} else if(instance.current_index < 0) {
+					instance.current_index = 0;
+				}
+				instance.selection(instance.current_index, 0);
+				instance.scroll_list_to(instance.current_index);
+				instance.store_active();
+			});
+			$(document).bind("keydown", "down", function() {
+				if(!instance.state_focus) {
+					return;
+				}
+				
+				count = 0;
+				instance.current_index++;
+				if(instance.current_index >= instance.all_items.size()) {
+					instance.current_index = instance.all_items.size() - 1;
+				} else if(instance.current_index < 0) {
+					instance.current_index = 0;
+				}
+				instance.selection(instance.current_index, 0);
+				instance.scroll_list_to(instance.current_index);
+				instance.store_active();
+			});
+			
+			this.blur();
+		},
+		
+		focus: function() {
+			if(this.state_focus) {
+				return;
+			}
+			this.state_focus = true;
+			
+			var instance = this;
+			var focus_bind = function(event) {
+				var element = $(event.target).closest(instance.options.sels.self);
+				if(element.size() == 0 || element.get(0) != instance.element.get(0)) {
+					instance.blur();
+				} else {
+					$("html").one("click", focus_bind);
+				}
+			};
+			$("html").one("click", focus_bind);
+			$("*").one("focus", function() {
+				instance.blur();
+			});
+		},
+		
+		blur: function() {
+			this.state_focus = false;
+			
+			var instance = this;
+			this.element.one("click", function(event) {
+				instance.focus();
+				$(event.target).trigger("click");
+			});
+		},
+		
+		selection: function(index, count, store) {
+			var size = this.all_items.size();
+			var from = Math.min(index, index + count);
+			var to = Math.max(index, index + count);
+			var change = false;
+			for(var i = 0; i < size; i++) {
+				if(i >= from && i <= to) {
+					if(this.activate(i)) {
+						change = true;
+					}
+				} else {
+					if(this.deactivate(i, store)) {
+						change = true;
+					}
+				}
+			}
+			return change;
+		},
+		
+		activate: function(index) {
+			var option = this.all_options.eq(index);
+			var li = this.all_items.eq(index);
+			if(!li.hasClass(this.options.classes.active)) {
+				option.attr("selected", "selected");
+				li.addClass(this.options.classes.active);
+				option.trigger("click");
+				return true;
+			}
+			return false;
+		},
+		
+		deactivate: function(index, store) {
+			if(store && this.list_active.indexOf(index) != -1) {
+				return false;
+			}
+			var option = this.all_options.eq(index);
+			var li = this.all_items.eq(index);
+			if(li.hasClass(this.options.classes.active)) {
+				li.removeClass(this.options.classes.active);
+				option.removeAttr("selected");
+				option.trigger("click");
+				return true;
+			}
+			return false;
+		},
+		
+		toggle: function(index) {
+			var option = this.all_options.eq(index);
+			var li = this.all_items.eq(index);
+			if(li.hasClass(this.options.classes.active)) {
+				option.removeAttr("selected");
+				li.removeClass(this.options.classes.active);
+				this.current_index = this.select.find("option:selected").last().index();
+			} else {
+				option.attr("selected", "selected");
+				li.addClass(this.options.classes.active);
+				this.current_index = index;
+			}
+			option.trigger("click");
+			this.select.trigger("change");
+		},
+		
+		scroll_list_to: function(index) {
+			this.list.scrollTo(this.all_items.eq(index));
+		},
+		
+		store_active: function() {
+			var instance = this;
+			this.list_active = [];
+			this.select.find("option:selected").each(function() {
+				instance.list_active.push($(this).index());
+			});
+		},
+		
+		restore_active: function() {
+			var change = false;
+			for(var i = 0; i < this.list_active.length; i++) {
+				if(this.activate(this.list_active[i])) {
+					change = true;
+				}
+			}
+			return change;
+		},
+		
+		destroy: function() {
+			this.element.unbind("click");
+			this.list.remove();
+			this.select.unwrap();
+			this.select.show();
+			$.Widget.prototype.destroy.call(this);
+		}
+	});
+})(jQuery, Array);
